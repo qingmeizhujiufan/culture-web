4ce1b70b58590163658b426def879dae0c85787d
钟正 <616028858@qq.com>
2018-07-25 17:28:14 +0800

    新增高德地图；

--- src/modules/contractUs/component/index.js
+++ src/modules/contractUs/component/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {Row, Col, Input, Icon, Tabs, Avatar, Breadcrumb, Badge, notification, Spin, message, Button} from 'antd';
-import _ from 'lodash';
+import {Map} from 'react-amap';
+import Marker from 'react-amap/lib/marker';
 import restUrl from 'RestUrl';
 import ajax from 'Utils/ajax';
 import '../index.less';
@@ -33,6 +34,7 @@ class Index extends React.Component {
                         <Row>
                             <Col span={7}>
                                 <div style={{
+                                    height: 615,
                                     padding: 42,
                                     border: '1px solid #DFDDDA',
                                     color: '#170202',
@@ -48,7 +50,21 @@ class Index extends React.Component {
                                 </div>
                             </Col>
                             <Col span={17}>
-
+                                <div style={{height: 437, marginTop: 89}}>
+                                    <Map
+                                        amapkey='540e9b2c0349b6a56b5c74d021aa28e5'
+                                        zoom={12}
+                                        plugins={['ToolBar']}
+                                        center={{longitude: 114.304063, latitude: 30.595826}}
+                                    >
+                                        <Marker
+                                            position={{
+                                                longitude: 114.304063,
+                                                latitude: 30.595826
+                                            }}
+                                        />
+                                    </Map>
+                                </div>
                             </Col>
                         </Row>
                     </div>
