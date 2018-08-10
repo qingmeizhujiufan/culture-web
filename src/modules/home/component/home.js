9ef4b05a58ad7013b5bb1d536b630a67c3487407
钟正 <616028858@qq.com>
2018-08-10 15:19:31 +0800

    修改首页；

--- src/modules/home/component/home.js
+++ src/modules/home/component/home.js
@@ -3,30 +3,15 @@ import PropTypes from 'prop-types';
 import {Layout, Menu, Icon, Row, Col, Steps, Progress, List, Button, Carousel} from 'antd';
 import '../index.css';
 import '../home.less';
-import banner1 from 'Img/test/banner1.png';
-import banner2 from 'Img/cover.jpg';
-import news from 'Img/test/news.jpg';
+import banner1 from 'Img/banner1.jpg';
+import banner2 from 'Img/banner2.jpg';
+import banner3 from 'Img/banner3.jpg';
 
 import restUrl from 'RestUrl';
 import ajax from 'Utils/ajax';
 
 const queryNewsListUrl = restUrl.ADDR + 'news/queryList';
 const queryHomeCulutreDetail = restUrl.ADDR + 'Server/queryHomeCulutreDetail';
-
-const data = [
-    {
-        title: '字字珠玑！习近平为青年传“经”授“诀”',
-    },
-    {
-        title: '韩提议送还在华川蜀埋葬志愿军遗骸2.4… ',
-    },
-    {
-        title: '韩进集团会长赵亮稿将受训，涉嫌贪污',
-    },
-    {
-        title: '字字珠玑！习近平为青年传“经”授“诀”',
-    },
-];
 
 class Index extends React.Component {
     constructor(props) {
@@ -99,19 +84,30 @@ class Index extends React.Component {
                         arrows
                         infinite
                     >
-                        {
-                            [1, 2, 3].map((item, index) => (
-                                <div key={index}>
-                                    <div className='banner' style={{backgroundImage: `url(${banner1})`}}>
-                                        <div className='carousel-content'>
-                                            <h1>湖北民俗文化</h1>
-                                            <p>专注名俗文化传播，传承湖北民俗文化</p>
-                                            <span className='follow'><Icon type="wechat"/> 关注公众号</span>
-                                        </div>
-                                    </div>
+                        <div>
+                            <div className='banner' style={{backgroundImage: `url(${banner1})`}}>
+                                <div className='carousel-content'>
+                                    <h1>湖北民俗文化</h1>
+                                    <p>专注名俗文化传播，传承湖北民俗文化</p>
                                 </div>
-                            ))
-                        }
+                            </div>
+                        </div>
+                        <div>
+                            <div className='banner' style={{backgroundImage: `url(${banner2})`}}>
+                                <div className='carousel-content'>
+                                    <h1>湖北民俗文化</h1>
+                                    <p>专注名俗文化传播，传承湖北民俗文化</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='banner' style={{backgroundImage: `url(${banner3})`}}>
+                                <div className='carousel-content'>
+                                    <h1>湖北民俗文化</h1>
+                                    <p>专注名俗文化传播，传承湖北民俗文化</p>
+                                </div>
+                            </div>
+                        </div>
                     </Carousel>
                 </div>
                 <div className='page-content'>
